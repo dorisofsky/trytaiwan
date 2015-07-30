a9f736f3937212c9a0fbe356f8063e8ea603d0ea
dorisofsky <dorisofsky@gmail.com>
2015-07-30 14:20:26 +0800

    scale(9000)

--- trytaiwan.js
+++ trytaiwan.js
@@ -11,7 +11,7 @@ function drawTaiwan(){
         // 改掉.["county"]成為.county
        
         var path = d3.geo.path().projection( // 路徑產生器
-          d3.geo.mercator().center([121,24]).scale(10000) // 座標變換函式
+          d3.geo.mercator().center([121,24]).scale(9000) // 座標變換函式
          );
 
         d3.select("svg").selectAll("path").data(features).enter().append("path").attr({
@@ -45,13 +45,6 @@ function drawTaiwan(){
           $('#panel').css({'top':mouseY,'left':mouseX}).fadeIn('slow');
         });
 
-        $("path").mouseout(function(){                   //panel 區塊跟隨滑鼠移動
-          $("path").mousemove( function(e) {
-           mouseX = e.pageX; 
-           mouseY = e.pageY;
-          });  
-          $('#panel').css({'top':mouseY,'left':mouseX}).fadeIn('slow');
-        });
 
       });
 };
