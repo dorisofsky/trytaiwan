3d2388d7452d6cf99e85811fda46c1dc0be7ea5e
dorisofsky <dorisofsky@gmail.com>
2015-07-31 15:51:06 +0800

    輸出值改成name

--- trytaiwan.js
+++ trytaiwan.js
@@ -85,7 +85,7 @@ function drawTaiwan(){
           d3.select(this).transition().duration(300).style("opacity", 1);
           div.transition().duration(300)
           .style("opacity", 1)
-          div.text(d.properties.T_Name+ " ─ 洪災數量：" + d.properties.flood) //+ " : " + rateById[d.TOWN_ID] d.properties.T_Name
+          div.text(name) //+ " : " + rateById[d.TOWN_ID] d.properties.T_Name
           .style("left", (d3.event.pageX) + "px")
           .style("top", (d3.event.pageY -30) + "px");
         });
