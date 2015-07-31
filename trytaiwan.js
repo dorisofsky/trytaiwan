46cb25b3856746a6d3c86577b335376c20babfe4
dorisofsky <dorisofsky@gmail.com>
2015-07-31 15:49:23 +0800

    想要讓mouseover運作

--- trytaiwan.js
+++ trytaiwan.js
@@ -80,7 +80,7 @@ function drawTaiwan(){
         .style("opacity", 0);
       });
 
-   /* $("path").mouseover(function(){                   //panel 區塊跟隨滑鼠移動
+    .on("mouseover", function() {                //panel 區塊跟隨滑鼠移動
         $("path").mousemove( function(e) {
           d3.select(this).transition().duration(300).style("opacity", 1);
           div.transition().duration(300)
@@ -89,7 +89,7 @@ function drawTaiwan(){
           .style("left", (d3.event.pageX) + "px")
           .style("top", (d3.event.pageY -30) + "px");
         });
-    });*/
+    });
 
   });
 };
