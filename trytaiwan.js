f3d079fab020a9d257dd770940963da5bced9b0d
dorisofsky <dorisofsky@gmail.com>
2015-07-31 14:20:44 +0800

    放了一個莫拉克網路使用狀況的csv

--- trytaiwan.js
+++ trytaiwan.js
@@ -5,60 +5,59 @@ $(document).ready(function() {
 
 function drawTaiwan(){
 
-     var width = 1024;
-     var height = 700;
+  var width = 1024;
+  var height = 700;
 
 
-     
-     /*var path = d3.geo.path().projection( // 路徑產生器
-       d3.geo.mercator().center([121,24]).scale(8000).translate([width / 2, height / 2]) // 座標變換函式
-      );*/ //用下面的東西，把這行分開寫。
 
-     var projection = d3.geo.mercator()
-      .center([121,24])
-      .scale(8000)
-      .translate([width / 2, height / 2]);
+  /*var path = d3.geo.path().projection( // 路徑產生器
+  d3.geo.mercator().center([121,24]).scale(8000).translate([width / 2, height / 2]) // 座標變換函式
+  );*/ //用下面的東西，把這行分開寫。
 
-     var path = d3.geo.path().projection(projection);
-      
+  var projection = d3.geo.mercator()
+  .center([121,24])
+  .scale(8000)
+  .translate([width / 2, height / 2]);
 
-     d3.json(" http://dorisofsky.github.io/taiwan_realtime2/county.json", function(topodata) {
-      //改掉json為自己推播的json網址
-
-        var features = topojson.feature(topodata, topodata.objects.county).features;
-        // 改掉.["county"]成為.county
-
-        d3.select("svg").selectAll("path").data(features).enter().append("path").attr({
-          d: path,
-          name: function(d){
-              return d.properties.C_Name;
-          },
-          fill:'#55AA00',
-          stroke: '#E0FBE7'
-        });
-/*
-        d3.select("#pathCanvas").append("path")         //縣市/行政區界線>>一直失敗不知為何，所以放棄了。
-          .datum(topojson.mesh(topodata, topodata.objects[type], function(a, b) { return a !== b ; }))
-          .attr("d", path)
-          .attr("id", "county-boundary");
-*/
-        d3.select("svg").selectAll("path").on("mouseenter", function() {          // title div 顯示滑鼠所指向的縣市/行政區
-            fill = $(this).attr("fill");
-            $(this).attr("fill", '#116493');
-            $('#title').html($(this).attr("name"));      
-            $('#panel').css({"height": "20px","width": "50px"});
-           }).on("mouseout", function() {
-             $(this).attr("fill", fill);
-          });
-
-        $("path").mouseover(function(){                   //panel 區塊跟隨滑鼠移動
-          $("path").mousemove( function(e) {
-           mouseX = e.pageX; 
-           mouseY = e.pageY;
-          });  
-          $('#panel').css({'top':mouseY,'left':mouseX}).fadeIn('slow');
-        });
+  var path = d3.geo.path().projection(projection);
 
 
+  d3.json(" http://dorisofsky.github.io/taiwan_realtime2/county.json", function(topodata) {
+  //改掉json為自己推播的json網址
+
+    var features = topojson.feature(topodata, topodata.objects.county).features;
+    // 改掉.["county"]成為.county
+
+    d3.select("svg").selectAll("path").data(features).enter().append("path").attr({
+      d: path,
+      name: function(d){
+          return d.properties.C_Name;
+      },
+      fill:'#55AA00',
+      stroke: '#E0FBE7'
+    });
+    /*
+    d3.select("#pathCanvas").append("path")         //縣市/行政區界線>>一直失敗不知為何，所以放棄了。
+      .datum(topojson.mesh(topodata, topodata.objects[type], function(a, b) { return a !== b ; }))
+      .attr("d", path)
+      .attr("id", "county-boundary");
+    */
+    d3.select("svg").selectAll("path").on("mouseenter", function() {          // title div 顯示滑鼠所指向的縣市/行政區
+        fill = $(this).attr("fill");
+        $(this).attr("fill", '#116493');
+        $('#title').html($(this).attr("name"));      
+        $('#panel').css({"height": "20px","width": "50px"});
+       }).on("mouseout", function() {
+         $(this).attr("fill", fill);
       });
+
+    $("path").mouseover(function(){                   //panel 區塊跟隨滑鼠移動
+      $("path").mousemove( function(e) {
+       mouseX = e.pageX; 
+       mouseY = e.pageY;
+      });  
+      $('#panel').css({'top':mouseY,'left':mouseX}).fadeIn('slow');
+    });
+
+  });
 };
